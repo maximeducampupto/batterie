adf9330731030252551838287118dab7de7f374b
Max <maximeducampupto@gmail.com>
2018-12-07 14:07:47 +0100

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-/*
-Brief Projet :
-
-  - Dans le fichier css, vous trouverez la classe .playing, vous utiliserez cette classe sur l'élément div correspondant
-  à la touche utilisée par l'utilisateur
-
-  - Ce projet utilise l'api audio en javascript, faites des recherches si vous ne la connaissez pas ( javascript+audio )
-
-  - Le challenge principal consiste à utiliser jQuery pour selectionner les éléments appropriés en fonction du keyCode
-  de la touche préssée par l'utilisateur
-
-A S D F G H J K L
-  BONUS : A la fin du projet , les musiciens devront nous faire une démonstration !
- */
-
 function playAnimation(element)
 {
     element.addClass('playing');
